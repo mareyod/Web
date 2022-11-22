b69cb9729649694473587eebfaf10018374cb68f
mareyod <115928937+mareyod@users.noreply.github.com>
2022-11-22 18:02:59 +0300

    Delete Kaint(React) directory

--- Kaint(React)/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../styles/Button.css";
-
-const Button = (props) => {
-
-    return <input className="Button" type="button" {...props}></input>;
-}
-export default Button;
-  
